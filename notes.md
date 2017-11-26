ad79abf48a2e3e86756e6a7bb6c8e7d7b369fe72
Gary Hendrick <gary.hendrick@gmail.com>
2017-11-25 19:11:44 -0500

    created cpu_times messages and supporting broadcaster

---
+++ notes.md
@@ -0,0 +1,7 @@
+Client machines may need to set the following environmental variable
+
+export ROS_IP=`hostname -I`
+
+This would be indicated by the rocore error 
+
+"Couldn't find an AF_INET address for [hostname]'
--- scripts/broadcaster.py
+++ scripts/broadcaster.py
@@ -3,16 +3,30 @@
 
 import rospy
 import psutil
-from std_msgs.msg import String
+from msg_experiments.msg import cpu_times
 
 def broadcaster():
-    pub_psutil = rospy.Publisher('psutil', String, queue_size=10)
+    pub_psutil = rospy.Publisher('psutil', cpu_times, queue_size=10)
     rospy.init_node('broadcaster', anonymous=True)
     rate=rospy.Rate(10) # 10 hz
     while not rospy.is_shutdown():
-        perf_str = "performance is performant %s" % rospy.get_time()
-        rospy.loginfo(perf_str)
-        pub_psutil.publish(perf_str)
+        msg = cpu_times()
+        state = psutil.cpu_times(True)
+        msg.user = [s.user for s in state if hasattr(s, 'user')]
+        msg.system = [s.system for s in state if hasattr(s, 'system')]
+        msg.idle = [s.idle for s in state if hasattr(s, 'idle')]
+        msg.nice = [s.nice for s in state if hasattr(s, 'nice')]
+        msg.iowait = [s.iowait for s in state if hasattr(s, 'iowait')]
+        msg.irq = [s.irq for s in state if hasattr(s, 'irq')]
+        msg.softirq = [s.softirq for s in state if hasattr(s, 'softirq')]
+        msg.steal = [s.steal for s in state if hasattr(s, 'steal')]
+        msg.guest = [s.guest for s in state if hasattr(s, 'guest')]
+        msg.guest_nice = [s.guest_nice for s in state if hasattr(s, 'guest_nice')]
+        msg.interrupt = [s.interrupt for s in state if hasattr(s, 'interrupt')]
+        msg.dpc = [s.dpc for s in state if hasattr(s, 'dpc')]
+        
+        rospy.loginfo(msg)
+        pub_psutil.publish(msg)
         rate.sleep()
 
 if __name__ == '__main__':
